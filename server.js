f5d26c7b87173b6e44e21957f41484d6050e072e
Léo <boudonleo@gmail.com>
2025-02-19 08:50:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connexion à MongoDB
-mongoose.connect('mongodb://localhost:27017/activity-tracking', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Base de données connectée'))
+// Récupérer l'URL de MongoDB depuis les variables d'environnement
+const mongoURI = process.env.MONGODB_URI;  // Remplace par le nom de ta variable d'environnement sur Railway
+
+// Connexion à MongoDB via Railway
+mongoose.connect(mongoURI, { useCreateIndex: true, useFindAndModify: false })
+  .then(() => console.log('Base de données connectée'))
   .catch((err) => console.log('Erreur de connexion à MongoDB', err));
 
 // Schéma de l'activité utilisateur
