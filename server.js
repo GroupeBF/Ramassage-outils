6a211fdc7569efe0f61f0b444ae50bf1e275c375
Léo <boudonleo@gmail.com>
2025-02-27 22:29:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 const app = express();
 const storage = multer.memoryStorage();
-const upload = multer({ storage: multer.memoryStorage() })
+const upload = multer({ storage });
 const PORT = process.env.PORT || 5000;
 
 // Middleware
@@ -18,7 +18,6 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(upload.none());
 
 // Connexion MongoDB (Railway)
 const mongoURI = process.env.MONGODB_URI;
@@ -67,21 +66,12 @@ const transporter = nodemailer.createTransport({
 // API pour envoyer un e-mail
 app.post('/send-email', upload.single('photo'), async (req, res) => {
     try {
-        console.log("Données reçues :", req.body);
-        console.log("Fichier reçu :", req.file);
+        console.log("Données reçues :", req.body || "Aucuns commentaires");
+        console.log("Fichier reçu :", req.file || "Aucun fichier reçu");
 
         if (!req.body.subject || !req.body.text) {
             return res.status(400).json({ error: 'Sujet et contenu requis' });
         }
-
-        // 📌 Configuration de l'email
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER, // Remplace par ton email
-                pass: process.env.EMAIL_PASS // Remplace par ton mot de passe (ou utilise un token d'application)
-            }
-        });
 
         let mailOptions = {
             from: process.env.EMAIL_USER,
